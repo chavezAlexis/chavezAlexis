3d1f3806a64cef44c4f6d4d15001bb83f5fa1ba0
chavezAlexis <78992181+chavezAlexis@users.noreply.github.com>
2022-10-03 14:19:43 -0500

    Add files via upload

---
+++ app.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// print array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    return 0;
+}
